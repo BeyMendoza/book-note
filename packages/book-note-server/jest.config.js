afeb16b8c7c36e0c39e3f05f114c3e9a9a6330bf
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2022-03-20 21:08:38 +0100

    feat [book-note-server]: Add Jest

---
+++ packages/book-note-server/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
